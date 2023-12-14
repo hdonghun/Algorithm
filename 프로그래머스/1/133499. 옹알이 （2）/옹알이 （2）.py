1ec63f986bddfc4baea354e223e1bc4b444960f1
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2023-12-14 21:54:06 +0900

    [level 1] Title: 옹알이 (2), Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,15 +1,18 @@
 def solution(babbling):
     answer = 0
-    baby = ["aya", "ye", "woo", "ma"]
-
+    result = ["aya", "ye", "woo", "ma" ]
     for i in babbling:
-        word = ''
-        a = ''
-        for b in i:
-            word += b # word에 한 글자씩 추가
-            if word != a and word in baby: # word가 중복되지 않고 baby안에 있다면
-                a = word
-                word = ''
-        if not word:
+        if '' in i.split('aya'):
+            print("i.split('aya')", i.split('aya'))
             answer += 1
+        elif '' in i.split('ye'):
+            print("i.split('ye')", i.split('ye'))
+            answer += 1
+        elif '' in i.split('woo'):
+            print("i.split('woo')", i.split('woo'))
+            answer += 1
+        elif '' in i.split('ma'):
+            print("i.split('ma')", i.split('ma'))
+            answer += 1
+    
     return answer
