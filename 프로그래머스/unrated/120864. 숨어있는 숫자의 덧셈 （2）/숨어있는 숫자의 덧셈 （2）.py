06a4875cbbc980ca09ce003439042468d7aef394
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2023-12-18 13:26:07 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,13 +1,11 @@
-import re
-def solution(my_string):
-    answer = 0
-    result = ''
-    
-    result = re.sub(r"[^0-9]", " ",my_string) #re.sub으로 숫자만 남기기
-    numbers = re.findall(r'\d+', result)
-    for i in range(len(numbers)):
-        answer += int(numbers[i])
-
-    print(numbers)
-            
-    return answer
+def solution(myString):
+    answer=0
+    # my_string='1a2b3c4d123Z'
+    for i in myString:
+        if i.isdigit():
+            pass
+        else:
+            myString=myString.replace(i,' ')
+        
+        
+    return sum(list(map(int,myString.split())))
