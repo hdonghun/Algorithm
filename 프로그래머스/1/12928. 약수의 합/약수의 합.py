b903f72da861a347e9ed0988e934b3908ea55ab8
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2023-12-26 20:47:14 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,6 +1,7 @@
 def solution(n):
     answer = 0
-    for i in range(1, n+1):
+    
+    for i in range(1,n+1):
         if n % i == 0:
-            answer += i
+            answer+=i
     return answer
