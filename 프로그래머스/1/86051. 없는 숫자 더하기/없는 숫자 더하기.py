ccd11882fbee4e7e01bf4647f90c65f2075e81ae
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2024-01-04 08:26:53 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,12 +1,9 @@
 def solution(numbers):
-    result = [0,1,2,3,4,5,6,7,8,9]
-    answer = 0
-
-    for i in numbers:
-        if i in result:
-            result.remove(i)
-
-    for i in result:
-        answer += i
-             
-    return answer
+    answer = [1,2,3,4,5,6,7,8,9]
+    result = 0
+    
+    for i in answer:
+        if i not in numbers:
+            result += i
+    
+    return result
