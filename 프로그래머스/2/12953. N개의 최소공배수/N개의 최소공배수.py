eb8107029ab70c5aea6190229bf8dbeeef5beee8
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2024-02-09 23:21:52 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -7,13 +7,9 @@ def solution(arr):
     return answer
 
 def calculate_lcm(a, b):
-    # 최소공배수를 계산하는 함수
-    # a와 b의 곱을 최대공약수로 나누면 최소공배수가 됨
     return a * b // calculate_gcd(a, b)
 
 def calculate_gcd(a, b):
-    # 최대공약수를 계산하는 함수
-    # 유클리드 호제법 사용
     while b:
         a, b = b, a % b
     return a
