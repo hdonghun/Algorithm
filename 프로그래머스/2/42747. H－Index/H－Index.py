a45727c564883028ab833bd28cf3455df77258a2
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2024-02-20 10:24:13 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -5,6 +5,7 @@ def solution(citations):
     h_index = 0
     for i in range(n):
         if citations[i] >= n - i:  
+            print('citations[i]',citations[i])
         # 현재 논문의 인용 횟수가 나머지 논문의 개수보다 크거나 같으면
             h_index = n - i  # h_index를 갱신
             break
