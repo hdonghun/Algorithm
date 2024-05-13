7b5e348fd3e4f2d381388fb650480cbb934de025
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2024-05-13 11:33:06 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#qna) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 20:41:2
+2024년 05월 13일 11:33:02
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,10 +1,21 @@
 def solution(n, lost, reserve):
-    lost ,reserve = list(set(lost) - set(reserve)) , list(set(reserve) - set(lost))
-    for r in reserve:
-        f = r - 1
-        b = r + 1
-        if f in lost:
-            lost.remove(f)
-        elif b in lost:
-            lost.remove(b)
-    return n - len(lost)
+    answer = 0
+    
+    lost.sort()
+    reserve.sort()
+    
+    # 1. 여분의 체육복을 가진 학생이 도난당한 경우를 처리
+    new_lost = [l for l in lost if l not in reserve]
+    new_reserve = [r for r in reserve if r not in lost]
+    
+    # 2. 남은 여분의 체육복을 가진 학생들이 도난당한 경우를 처리
+    for r in new_reserve:
+        if r - 1 in new_lost:
+            new_lost.remove(r - 1)
+        elif r + 1 in new_lost:
+            new_lost.remove(r + 1)
+    
+    # 3. 남은 여분의 체육복을 가진 학생들이 도난당하지 않은 경우를 처리
+    answer = n - len(new_lost)
+    
+    return answer
