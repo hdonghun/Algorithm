60708c01783ab751eb4c1a41178f2dec3d1cdb17
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2024-05-29 18:41:45 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -4,17 +4,26 @@ def solution(begin, target, words):
     if target not in words:
         return 0
     
+    # 한 글자 차이인지 확인하는 함수
+    def one_letter_diff(word1, word2):
+        return sum([c1 != c2 for c1, c2 in zip(word1, word2)]) == 1
+    
+    # BFS 초기화
     queue = deque([(begin, 0)])
-    visited = set()
+    visited = set([begin])
     
     while queue:
-        word, level = queue.popleft()
-        if word == target:
-            return level
+        current_word, steps = queue.popleft()
         
-        for next_word in words:
-            if next_word not in visited and sum(char1 != char2 for char1, char2 in zip(word, next_word)) == 1:
-                visited.add(next_word)
-                queue.append((next_word, level + 1))
-                
+        # target에 도달하면 현재까지의 변환 횟수 반환
+        if current_word == target:
+            return steps
+        
+        # 현재 단어에서 한 글자만 다른 단어들을 찾아 큐에 추가
+        for word in words:
+            if word not in visited and one_letter_diff(current_word, word):
+                visited.add(word)
+                queue.append((word, steps + 1))
+    
+    # target에 도달하지 못하면 변환 불가
     return 0
