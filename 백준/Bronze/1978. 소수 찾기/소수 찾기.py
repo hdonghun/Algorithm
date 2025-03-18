92cabb62de25f73efb5fc52e7cb0cbb00a91002f
한동훈 <67058000+hdonghun@users.noreply.github.com>
2025-03-18 21:04:35 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -3,11 +3,15 @@ data = list(map(int, input().split()))
 count = 0
 
 for x in data:
-  for i in range(2, x+1):
-    if x % i == 0:
-      if x == i:
+    if x < 2:
+        continue
+    is_prime = True
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            is_prime = False
+            break
+    
+    if is_prime:
         count += 1
-      
-      break
 
 print(count)
