82e0bd31d17fedf0d88a0f15684ba14f90d78645
한동훈 <67058000+hdonghun@users.noreply.github.com>
2025-06-17 22:09:56 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,14 +1,12 @@
-temp = []
-tt = ''
-
+data = []
 for i in range(5):
-    temp.append(list(input()))
+    line = input()
+    data.append(line)
 
-max_len = max(map(len, temp))
+result = ''
+for j in range(15): # 문제에서 명시적으로 "문자열 길이는 최대 15자까지 올 수 있다"고 주어짐 
+    for i in range(5):
+        if j < len(data[i]): # 지금 읽으려는 인덱스 j가 해당 줄의 길이보다 작을 때만 접근
+            result += data[i][j]
 
-for i in range(max_len):
-    for j in range(5):
-        if i < len(temp[j]): 
-            tt += temp[j][i]
-
-print(tt) 
+print(result)
